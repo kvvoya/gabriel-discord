f7c802dc274d2031d8da5450fe9ad503ce96997f
kvvoya <suruhakken@gmail.com>
2023-05-22 16:38:10 +0300

    i guess it's finished?

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { config } = require('dotenv');
+
+config();
+
+const { BOT_TOKEN, SINNER_STICKER_NAME, GUARD_CHANNEL_ID, REMOVE_SINNER_MESSAGE, BAN_SINNER } = process.env; // .env
+
+const removeSinnerMessage = REMOVE_SINNER_MESSAGE === 'true';
+const banSinner = BAN_SINNER === 'true';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+   console.log(`Bot logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+   if (message.author.bot) return;
+
+   if (message.channelId === GUARD_CHANNEL_ID) {
+      // console.log(message);
+   
+      let isValid = false;
+
+      const sticker = message.stickers;
+      let stickerName = null;
+   
+      for (const stick of sticker.values()) {
+         stickerName = stick.name;
+      }
+   
+      isValid = stickerName === SINNER_STICKER_NAME;
+
+      if (!isValid) {
+         const user = message.author;
+         user.send('Human! You will not get away with this! I see what you tried to do! You wanted to break the holy rule of #may-your-ls-be-many-and-bitches-few \
+However, I see everything, and I have seen, what you did there... As a result, your sinful message has been removed. May your L\'s be many and your bitches few...');
+
+         if (removeSinnerMessage) {
+            message.delete();
+         }
+
+         if (banSinner) {
+            try {
+               await message.member.ban();
+               user.send('Also you have been banned, LMAO!');
+            } catch (err) {
+               console.error('Failed to ban user!', err);
+            }
+         }
+      }
+   }
+});
+
+client.login(BOT_TOKEN);
